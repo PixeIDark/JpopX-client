e5233f0c4ae3b38e375fa5ea32cb6f407e1d1dc3
PixeIDark <kiki7951@naver.com>
2025-02-25 08:40:53 +0900

    회원가입 페이지 시작

---
+++ src/app/(auth)/account/_components/AccountForm/AccountForm.tsx
@@ -0,0 +1,5 @@
+function AccountForm() {
+  return <div>어카폼</div>;
+}
+
+export default AccountForm;
---
+++ src/app/(auth)/account/page.tsx
@@ -0,0 +1,11 @@
+import AccountForm from "@/app/(auth)/account/_components/AccountForm";
+
+function AccountPage() {
+  return (
+    <div>
+      <AccountForm />
+    </div>
+  );
+}
+
+export default AccountPage;
