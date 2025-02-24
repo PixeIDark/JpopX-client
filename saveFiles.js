cedf08c3acb96ed0723a80b7f610fe6d0f4faf14
PixeIDark <kiki7951@naver.com>
2025-02-25 08:37:36 +0900

    zod, lucide, axios, hookform 설치

--- saveFiles.js
+++ saveFiles.js
@@ -1,17 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function saveFilesAsTxt(directory, outputFile) {
   const files = fs.readdirSync(directory);
-  let outputContent = '';
+  let outputContent = "";
 
-  files.forEach(file => {
+  files.forEach((file) => {
     const fullPath = path.join(directory, file);
     const stat = fs.statSync(fullPath);
     if (stat.isDirectory()) {
       saveFilesAsTxt(fullPath, outputFile); // 재귀적으로 하위 폴더도 탐색
-    } else if (file.endsWith('.js') || file.endsWith('.ts') || file.endsWith('.jsx') || file.endsWith('.tsx')) { // 파일 확장자 지정
-      const fileContent = fs.readFileSync(fullPath, 'utf-8');
+    } else if (
+      file.endsWith(".js") ||
+      file.endsWith(".ts") ||
+      file.endsWith(".jsx") ||
+      file.endsWith(".tsx")
+    ) {
+      // 파일 확장자 지정
+      const fileContent = fs.readFileSync(fullPath, "utf-8");
       outputContent += `--- ${fullPath} ---\n${fileContent}\n\n`;
     }
   });
@@ -20,4 +26,4 @@ function saveFilesAsTxt(directory, outputFile) {
 }
 
 // src 폴더 경로와 출력될 텍스트 파일 경로 지정
-saveFilesAsTxt('src', 'server.txt');
+saveFilesAsTxt("src", "client.txt");
