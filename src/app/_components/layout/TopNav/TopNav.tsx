50966c731f4a6586964c8ddd1012b4c26e1e783c
PixeIDark <kiki7951@naver.com>
2025-02-25 16:54:42 +0900

    TopNav.tsx 생성

---
+++ src/app/_components/layout/TopNav/TopNav.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ArrowLeft } from "lucide-react";
+import ThemeToggle from "@/app/_components/ThemeToggle";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+
+const NAV_TITLE = {
+  "/": "Home",
+  "/login": "Login",
+  "/account": "Account",
+} as const;
+
+type NavPath = keyof typeof NAV_TITLE;
+
+function TopNav() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const title = pathname in NAV_TITLE ? NAV_TITLE[pathname as NavPath] : "Unknown Page";
+
+  return (
+    <div className="pt sticky top-0 -mx-4 flex items-center justify-between border-b border-solid-default bg-body-default px-4 pb-3 pt-5">
+      <button onClick={() => router.back()}>
+        <ArrowLeft size={24} strokeWidth={1.5} />
+      </button>
+      <Link href="/">
+        <h1 className="text-lg font-semibold text-text-h">{title}</h1>
+      </Link>
+      <ThemeToggle />
+    </div>
+  );
+}
+
+export default TopNav;
