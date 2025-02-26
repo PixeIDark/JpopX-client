0b39f1e9455c478710607d9e6cce461a668f01f4
PixeIDark <kiki7951@naver.com>
2025-02-26 13:39:26 +0900

    instance에서 session 잘가져오게 수정

--- src/lib/axios/instance.ts
+++ src/lib/axios/instance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { getServerSession } from "next-auth";
 import { RefreshRequest, RefreshResponse } from "@/types/auth.type";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/next-auth/nextAuth";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -15,8 +16,7 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   async function (config) {
     try {
-      const session = await getServerSession();
-
+      const session = await getServerSession(authOptions);
       if (session?.user?.accessToken) {
         config.headers.Authorization = `Bearer ${session.user.accessToken}`;
       }
@@ -59,6 +59,7 @@ axiosInstance.interceptors.response.use(
     const originalRequest = error.config;
 
     if (originalRequest._retry === 3) {
+      await signOut({ redirect: true, callbackUrl: "/login?error=RefreshTokenFailed" });
       return Promise.reject(error);
     }
 
@@ -67,7 +68,7 @@ axiosInstance.interceptors.response.use(
       originalRequest._retry++;
 
       try {
-        const session = await getServerSession();
+        const session = await getServerSession(authOptions);
 
         if (!session?.user?.refreshToken) {
           // 리프레시 토큰이 없으면 로그인 페이지로 리디렉션 로직 추가 가능
@@ -88,6 +89,8 @@ axiosInstance.interceptors.response.use(
         return axiosInstance(originalRequest);
       } catch (refreshError) {
         // 리프레시 실패 시 로그아웃 처리 등 구현 가능
+        // 로그아웃으로 세션 무효화 => 이후 next-auth pages 설정에 따라 리다이렉트됨
+        await signOut({ redirect: true, callbackUrl: "/login?error=RefreshTokenFailed" });
         console.error("Token refresh failed:", refreshError);
         return Promise.reject(refreshError);
       }
