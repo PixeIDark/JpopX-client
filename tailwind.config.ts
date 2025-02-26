4b67319e7475d5723ddece9bd81b36a79204b866
PixeIDark <kiki7951@naver.com>
2025-02-26 16:38:21 +0900

    toast 키 프레임 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ module.exports = {
   darkMode: ["class", '[data-theme="dark"]'],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateX(20px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-out forwards",
+      },
       colors: {
         text: {
           h: "var(--color-text-h)",
