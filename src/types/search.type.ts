6b20153e94b14ec16c59d02f2348faf8ba8d4974
PixeIDark <kiki7951@naver.com>
2025-03-01 11:10:17 +0900

    search.type.ts text 타입 undefined 추가

--- src/types/search.type.ts
+++ src/types/search.type.ts
@@ -2,7 +2,7 @@ export type SearchType = "both" | "artist" | "title" | "lyrics";
 export type Sort = "latest" | "popular";
 
 export interface SearchRequest {
-  text: string;
+  text?: string;
   searchType?: SearchType;
   sort?: Sort;
   limit?: number;
@@ -11,26 +11,28 @@ export interface SearchRequest {
 
 export type SearchPanelParams = Omit<SearchRequest, "limit" | "page">;
 
+export interface SearchItems {
+  id: number;
+  song_id: number;
+  title_ko: string;
+  title_ja?: string;
+  title_en?: string;
+  artist_id: number;
+  release_date?: string;
+  thumbnail_url: string | null;
+  popularity_score: number;
+  created_at: string;
+  updated_at: string;
+  artist_ko?: string;
+  artist_ja?: string;
+  artist_en?: string;
+  romanized_ko?: string;
+  tj_number?: string;
+  kumyoung_number?: string;
+}
+
 export interface SearchResponse {
-  items: {
-    id: number;
-    song_id: number;
-    title_ko: string;
-    title_ja?: string;
-    title_en?: string;
-    artist_id: number;
-    release_date?: string;
-    thumbnail_url?: string;
-    popularity_score: number;
-    created_at: string;
-    updated_at: string;
-    artist_ko?: string;
-    artist_ja?: string;
-    artist_en?: string;
-    romanized_ko?: string;
-    tj_number?: string;
-    kumyoung_number?: string;
-  }[];
+  items: SearchItems[];
   total: number;
   page: number;
   limit: number;
