fc68cd1be37af2922d72255d162690621af06764
PixeIDark <kiki7951@naver.com>
2025-03-02 21:30:14 +0900

    item 객체로 넘김

--- src/app/_components/layout/BottomNav/BottomNav.tsx
+++ src/app/_components/layout/BottomNav/BottomNav.tsx
@@ -30,13 +30,7 @@ function BottomNav() {
     <div>
       <div className="pt fixed bottom-0 flex w-full items-center justify-between border-t border-solid-default bg-body-default px-4 pb-3 pt-5">
         {navItems.map((item) => (
-          <BottomNavItem
-            key={item.path}
-            path={item.path}
-            label={item.label}
-            Icon={item.Icon}
-            IconFill={item.IconFill}
-          />
+          <BottomNavItem key={item.path} item={item} />
         ))}
       </div>
       <div className="h-[77px] w-full" />
