8680fce877992f048f3f09a16f66bcb7fab0ed15
PixeIDark <kiki7951@naver.com>
2025-03-03 12:48:36 +0900

    훅 규칙 어김.. 훅 호출 전에 있던 얼리 이프문 제거

--- src/app/search/_components/SearchResult/SearchResult.tsx
+++ src/app/search/_components/SearchResult/SearchResult.tsx
@@ -14,27 +14,24 @@ interface SearchResultProps {
 const DISPLAY_COUNT = 2;
 
 function SearchResult({ params }: SearchResultProps) {
-  if (!params || params.text === undefined) return null;
-
   const { loadMoreRef, isLoading, songs, isFetchingNextPage, searchTotal } = useSearch({
     ...params,
-    text: params.text, // 그냥 params 넣으면 params.text 는 string 인게 검증되었는데도 뭐라고함
+    text: params.text || "",
   });
 
   if (isLoading) return <SearchLoading isLoading={isLoading} />;
-
   if (!songs.length) return <NotFound text="Don't have any songs!" />;
 
-  const displayLimit = songs.length - DISPLAY_COUNT;
+  const displayLimit = Math.max(0, songs.length - DISPLAY_COUNT);
   const displayedSongs = songs.slice(0, displayLimit);
   const overflowSongs = songs.slice(displayLimit);
 
   return (
     <div className="mt-2">
       <p className="mb-2 text-sm font-normal text-text-p">{searchTotal} results</p>
-      <SearchList songs={displayedSongs} />
+      {displayedSongs.length > 0 && <SearchList songs={displayedSongs} />}
       <div ref={loadMoreRef} className="h-0 w-full py-3" />
-      <SearchList songs={overflowSongs} />
+      {overflowSongs.length > 0 && <SearchList songs={overflowSongs} />}
       {isFetchingNextPage && <LoadingSpinner isLoading={isFetchingNextPage} />}
     </div>
   );
