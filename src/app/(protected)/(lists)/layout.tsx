7f44ecf7719e4ffeb580c81697a27611e33fa50d
PixeIDark <kiki7951@naver.com>
2025-03-04 12:26:53 +0900

    저장

--- src/app/(protected)/(lists)/layout.tsx
+++ src/app/(protected)/(lists)/layout.tsx
@@ -1,14 +1,25 @@
+"use server";
+
 import React from "react";
 import Button from "@/components/ui/Button";
+import { getServerQueryClient } from "@/lib/tanStackQuery/getServerQueryClient";
+import { favoriteListsOption } from "@/query/favorite-lists/options/favoriteListsOption";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 async function ListsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = getServerQueryClient();
+
+  await queryClient.prefetchQuery(favoriteListsOption);
+
   return (
     <div className="flex flex-col">
-      <div className="overflow-y-auto">{children}</div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="overflow-y-auto">{children}</div>
+      </HydrationBoundary>
       <Button variant="active">Create New List</Button>
     </div>
   );
