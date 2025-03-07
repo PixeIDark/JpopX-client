2af8bdfc941ade37ca3b9e7774fa23eea399a5a3
PixeIDark <kiki7951@naver.com>
2025-03-07 23:51:11 +0900

    suspense로 바꿈

--- src/query/favorite-lists/queries/useFavoriteListSongsQuery.ts
+++ src/query/favorite-lists/queries/useFavoriteListSongsQuery.ts
@@ -1,11 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { favoriteListsApi } from "@/api/favorite-lists";
 import { getFavoriteListSongsKey } from "@/query/favorite-lists";
 
 export function useFavoriteListSongsQuery(listId: number) {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: getFavoriteListSongsKey(listId),
     queryFn: () => favoriteListsApi.getListSongs(listId),
-    enabled: !!listId,
   });
 }
--- src/query/favorite-lists/queries/useFavoriteListsQuery.ts
+++ src/query/favorite-lists/queries/useFavoriteListsQuery.ts
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { favoriteListsApi } from "@/api/favorite-lists";
 import { getFavoriteListsKey } from "@/query/favorite-lists";
 
 export function useFavoriteListsQuery() {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: getFavoriteListsKey(),
     queryFn: () => favoriteListsApi.getLists(),
   });
