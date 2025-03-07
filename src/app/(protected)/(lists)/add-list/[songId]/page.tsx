6069072ab9071b67c2e4730c495c42ac9354fe66
PixeIDark <kiki7951@naver.com>
2025-03-07 23:51:39 +0900

    하이드레이션 전용 옵션 만들고 사용

--- src/app/(protected)/(lists)/add-list/[songId]/page.tsx
+++ src/app/(protected)/(lists)/add-list/[songId]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { favoriteListsOption } from "@/query/favorite-lists/options/favoriteListsOption";
 import { use } from "react";
 import AddToList from "@/app/(protected)/(lists)/add-list/[songId]/_components/AddToList";
 
 function AddListPage({ params }: { params: Promise<{ songId: string }> }) {
-  const { data: lists } = useQuery(favoriteListsOption);
+  const { data: lists } = useSuspenseQuery(favoriteListsOption);
   const { songId } = use(params);
 
   if (!lists || !songId) return null;
