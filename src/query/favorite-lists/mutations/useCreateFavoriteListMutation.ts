581cf05da3a2e05b0405695095dcf68b8999b687
PixeIDark <kiki7951@naver.com>
2025-03-07 09:29:02 +0900

    리스트 생성 낙관적 ui 적용

--- src/query/favorite-lists/mutations/useCreateFavoriteListMutation.ts
+++ src/query/favorite-lists/mutations/useCreateFavoriteListMutation.ts
@@ -1,8 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { favoriteListsApi } from "@/api/favorite-lists";
-import { CreateFavoriteListRequest } from "@/types/favorite-list.type";
+import { CreateFavoriteListRequest, FavoriteList } from "@/types/favorite-list.type";
 import { useToast } from "@/components/ui/Toast/useToast";
 import { getFavoriteListsKey } from "@/query/favorite-lists";
+import { getRandomInteger } from "@/utils/getRandomInteger";
 
 export function useCreateFavoriteListMutation() {
   const queryClient = useQueryClient();
@@ -10,20 +11,43 @@ export function useCreateFavoriteListMutation() {
 
   return useMutation({
     mutationFn: (data: CreateFavoriteListRequest) => favoriteListsApi.createList(data),
+    onMutate: (data) => {
+      queryClient.cancelQueries({ queryKey: getFavoriteListsKey() });
+      const previousLists = queryClient.getQueryData<FavoriteList[]>(getFavoriteListsKey());
+
+      if (previousLists) {
+        const newList = {
+          id: -1,
+          user_id: -getRandomInteger(),
+          name: data.name,
+          order: previousLists[previousLists.length - 1].order + 1,
+          image_url: null,
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
+          deleted_at: null,
+        };
+        queryClient.setQueryData(getFavoriteListsKey(), [...previousLists, newList]);
+
+        return { previousLists };
+      }
+    },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: getFavoriteListsKey() });
       toast({
         title: "My List Created Successfully",
         message: "My List Created Successfully",
         type: "success",
       });
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context?.previousLists)
+        queryClient.setQueryData(getFavoriteListsKey(), context.previousLists);
+
       toast({
         title: "Error",
         message: error instanceof Error ? error.message : "Failed To Create My List",
         type: "error",
       });
     },
+    onSettled: () => queryClient.invalidateQueries({ queryKey: getFavoriteListsKey() }),
   });
 }
