1dfe0ff15ab3241977123fa064ba2bdd14954a3a
PixeIDark <kiki7951@naver.com>
2025-03-07 23:52:37 +0900

    낙관성 ui 추가

--- src/query/favorite-lists/mutations/useRemoveSongFromListMutation.ts
+++ src/query/favorite-lists/mutations/useRemoveSongFromListMutation.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { favoriteListsApi } from "@/api/favorite-lists";
 import { useToast } from "@/components/ui/Toast/useToast";
 import { getFavoriteListSongsKey } from "@/query/favorite-lists";
+import { FavoriteListSong } from "@/types/favorite-list.type";
 
 export function useRemoveSongFromListMutation(listId: number) {
   const queryClient = useQueryClient();
@@ -9,20 +10,37 @@ export function useRemoveSongFromListMutation(listId: number) {
 
   return useMutation({
     mutationFn: (favoriteId: number) => favoriteListsApi.removeSongFromList(favoriteId),
+    onMutate: async (favoriteId) => {
+      await queryClient.cancelQueries({ queryKey: getFavoriteListSongsKey(listId) });
+      const previousSongs = queryClient.getQueryData<FavoriteListSong[]>(
+        getFavoriteListSongsKey(listId)
+      );
+
+      if (previousSongs) {
+        queryClient.setQueryData(
+          getFavoriteListSongsKey(listId),
+          previousSongs.filter((song) => song.id !== favoriteId)
+        );
+      }
+      return { previousSongs };
+    },
     onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: getFavoriteListSongsKey(listId) });
       toast({
         title: "Song Removed From List Successfully",
         message: data.message,
         type: "success",
       });
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context?.previousSongs)
+        queryClient.setQueryData(getFavoriteListSongsKey(listId), context.previousSongs);
+
       toast({
         title: "Error",
         message: error instanceof Error ? error.message : "Failed To Remove Song From List",
         type: "error",
       });
     },
+    onSettled: () => queryClient.invalidateQueries({ queryKey: getFavoriteListSongsKey(listId) }),
   });
 }
