a20ca0455aa03493114551a2b996bae2eb3445f7
PixeIDark <kiki7951@naver.com>
2025-03-10 23:36:48 +0900

    인풋 컴포넌트 className 프롭 적용되게함

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -4,7 +4,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
       type={type}
-      className={`${className || ""}bg-input-input flex h-14 w-full items-center justify-center rounded-xl p-4 text-base text-text-h placeholder:text-text-p focus:outline-none focus:ring-1 focus:ring-solid-default`}
+      className={`${className || ""} flex h-14 w-full items-center justify-center rounded-xl bg-input-input p-4 text-base text-text-h placeholder:text-text-p focus:outline-none focus:ring-1 focus:ring-solid-default`}
       {...props}
     />
   );
