3080eace2ff1a6d08edc1106a665eba347269abd
PixeIDark <kiki7951@naver.com>
2025-03-10 23:37:01 +0900

    잡

--- src/components/common/Field/ErrorMessage/ErrorMessage.tsx
+++ src/components/common/Field/ErrorMessage/ErrorMessage.tsx
@@ -3,7 +3,7 @@ import { FieldError } from "react-hook-form";
 function ErrorMessage({ error }: { error: FieldError | undefined }) {
   if (!error) return null;
 
-  return <p className="text-button-error mt-1 text-sm">{error.message}</p>;
+  return <p className="mt-1 text-sm text-button-error">{error.message}</p>;
 }
 
 export default ErrorMessage;
