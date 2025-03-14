9556a8659a3eb31c856ba236fb5804d4680b71fc
PixeIDark <kiki7951@naver.com>
2025-03-15 00:57:18 +0900

    로그인 시, 수동으로 리다이렉트 설정. 로그인 인증 실패 시, 리다이렉트 막음

--- src/query/auth/mutations/useLoginMutation.ts
+++ src/query/auth/mutations/useLoginMutation.ts
@@ -2,20 +2,20 @@ import { useMutation } from "@tanstack/react-query";
 import { LoginRequest } from "@/types/auth.type";
 import { signIn } from "next-auth/react";
 import { useToast } from "@/components/ui/Toast/useToast";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export function useLoginMutation() {
   const { toast } = useToast();
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
+  const router = useRouter();
 
   return useMutation({
     mutationFn: async (data: LoginRequest) => {
       const result = await signIn("credentials", {
         email: data.email,
         password: data.password,
-        redirect: true,
-        callbackUrl,
+        redirect: false,
       });
 
       if (result?.error) {
@@ -23,6 +23,7 @@ export function useLoginMutation() {
       }
     },
     onSuccess: () => {
+      router.push(callbackUrl);
       toast({
         title: "Login Success",
         message: "Login Successfully Great!",
