a8a61d8fd8bec2939b8111fbd642158fdfa62c30
PixeIDark <kiki7951@naver.com>
2025-03-25 17:48:43 +0900

    인증 상태에따라 확실하게 렌더링

--- src/app/_layout/BottomNav/BottomNav.tsx
+++ src/app/_layout/BottomNav/BottomNav.tsx
@@ -8,18 +8,25 @@ import {
   LOGIN_ITEM,
   PROFILE_ITEM,
 } from "@/app/_layout/BottomNav/_constants/nav-item";
+import { useEffect, useState } from "react";
+
+type NavItem = (typeof BASE_NAV_ITEMS)[number] | typeof PROFILE_ITEM | typeof LOGIN_ITEM;
 
 function BottomNav() {
   const segment = useSelectedLayoutSegments();
   const { status } = useSession();
 
-  if (segment[0] === "(auth)" || segment[1] === "profile") return null;
+  const [navItems, setNavItems] = useState<NavItem[]>([...BASE_NAV_ITEMS]);
 
-  const NAV_ITEMS = [...BASE_NAV_ITEMS, status === "authenticated" ? PROFILE_ITEM : LOGIN_ITEM];
+  useEffect(() => {
+    setNavItems([...BASE_NAV_ITEMS, status === "authenticated" ? PROFILE_ITEM : LOGIN_ITEM]);
+  }, [status]);
+
+  if (segment[0] === "(auth)" || segment[1] === "profile") return null;
 
   return (
     <div className="fixed inset-x-0 bottom-0 z-10 mx-auto flex w-full max-w-lg items-center justify-between border-t border-solid-default bg-body-default px-4 pb-3 pt-5">
-      {NAV_ITEMS.map((item) => (
+      {navItems.map((item) => (
         <BottomNavItem key={item.path} item={item} />
       ))}
     </div>
