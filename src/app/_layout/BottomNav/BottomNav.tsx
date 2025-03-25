96011bb5754d5f7aed261e758a99e2944949c527
PixeIDark <kiki7951@naver.com>
2025-03-25 17:57:58 +0900

    인증 삭제

--- src/app/_layout/BottomNav/BottomNav.tsx
+++ src/app/_layout/BottomNav/BottomNav.tsx
@@ -8,25 +8,17 @@ import {
   LOGIN_ITEM,
   PROFILE_ITEM,
 } from "@/app/_layout/BottomNav/_constants/nav-item";
-import { useEffect, useState } from "react";
-
-type NavItem = (typeof BASE_NAV_ITEMS)[number] | typeof PROFILE_ITEM | typeof LOGIN_ITEM;
 
 function BottomNav() {
   const segment = useSelectedLayoutSegments();
   const { status } = useSession();
-  const [navItems, setNavItems] = useState<NavItem[]>([...BASE_NAV_ITEMS]);
-  console.log(status);
-
-  useEffect(() => {
-    setNavItems([...BASE_NAV_ITEMS, status === "authenticated" ? PROFILE_ITEM : LOGIN_ITEM]);
-  }, [status]);
-
   if (segment[0] === "(auth)" || segment[1] === "profile") return null;
+
+  const NAV_ITEMS = [...BASE_NAV_ITEMS, status === "authenticated" ? PROFILE_ITEM : LOGIN_ITEM];
 
   return (
     <div className="fixed inset-x-0 bottom-0 z-10 mx-auto flex w-full max-w-lg items-center justify-between border-t border-solid-default bg-body-default px-4 pb-3 pt-5">
-      {navItems.map((item) => (
+      {NAV_ITEMS.map((item) => (
         <BottomNavItem key={item.path} item={item} />
       ))}
     </div>
