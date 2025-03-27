fc98e1f34c7c495d5b523261cddb7f248b65c11f
PixeIDark <kiki7951@naver.com>
2025-03-27 19:47:35 +0900

    바텀 내비 useSession => hasSessionExpired

--- src/app/_layout/BottomNav/BottomNav.tsx
+++ src/app/_layout/BottomNav/BottomNav.tsx
@@ -2,19 +2,25 @@
 
 import BottomNavItem from "@/app/_layout/BottomNav/BottomNavItem";
 import { useSelectedLayoutSegments } from "next/navigation";
-import { useSession } from "next-auth/react";
 import {
   BASE_NAV_ITEMS,
   LOGIN_ITEM,
   PROFILE_ITEM,
 } from "@/app/_layout/BottomNav/_constants/nav-item";
+import { useEffect, useState } from "react";
+import { hasSessionExpired } from "@/utils/hasSessionExpired";
 
 function BottomNav() {
   const segment = useSelectedLayoutSegments();
-  const { status } = useSession();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    hasSessionExpired().then((res) => setIsAuthenticated(res));
+  }, []);
+
   if (segment[0] === "(auth)" || segment[1] === "profile") return null;
 
-  const NAV_ITEMS = [...BASE_NAV_ITEMS, status === "authenticated" ? PROFILE_ITEM : LOGIN_ITEM];
+  const NAV_ITEMS = [...BASE_NAV_ITEMS, isAuthenticated ? PROFILE_ITEM : LOGIN_ITEM];
 
   return (
     <div className="fixed inset-x-0 bottom-0 z-10 mx-auto flex w-full max-w-lg items-center justify-between border-t border-solid-default bg-body-default px-4 pb-3 pt-5">
