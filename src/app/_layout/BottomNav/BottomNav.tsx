1547158d4bcf30065c60a30207e3443a334e4991
PixeIDark <kiki7951@naver.com>
2025-03-28 12:01:12 +0900

    쿠키로 전환 아이콘 안깜빡거림

--- src/app/_layout/BottomNav/BottomNav.tsx
+++ src/app/_layout/BottomNav/BottomNav.tsx
@@ -7,16 +7,19 @@ import {
   LOGIN_ITEM,
   PROFILE_ITEM,
 } from "@/app/_layout/BottomNav/_constants/nav-item";
-import { useSession } from "next-auth/react";
 
-function BottomNav() {
+export type Status = "authenticated" | "unauthenticated";
+
+interface BottomNavProps {
+  status: Status;
+}
+
+function BottomNav({ status }: BottomNavProps) {
   const segment = useSelectedLayoutSegments();
-  const { data: session } = useSession();
-  const isAuthenticated = (session?.refreshTokenExpires ?? 0) > Date.now();
 
   if (segment[0] === "(auth)" || segment[1] === "profile") return null;
 
-  const NAV_ITEMS = [...BASE_NAV_ITEMS, isAuthenticated ? PROFILE_ITEM : LOGIN_ITEM];
+  const NAV_ITEMS = [...BASE_NAV_ITEMS, status === "authenticated" ? PROFILE_ITEM : LOGIN_ITEM];
 
   return (
     <div className="fixed inset-x-0 bottom-0 z-10 mx-auto flex w-full max-w-lg items-center justify-between border-t border-solid-default bg-body-default px-4 pb-3 pt-5">
