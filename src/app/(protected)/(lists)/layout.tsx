bc7f1a7c28c04f619911c434635298af4aeb1e07
PixeIDark <kiki7951@naver.com>
2025-04-13 09:06:00 +0900

    실험적 suspense (lists)/layout.tsx 적용 어떤식으로 관리해야될지 못 정함

--- src/app/(protected)/(lists)/layout.tsx
+++ src/app/(protected)/(lists)/layout.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { getServerQueryClient } from "@/lib/tanStackQuery/getServerQueryClient";
 import { favoriteListsOption } from "@/query/favorite-lists/options/favoriteListsOption";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 
 export const dynamic = "force-dynamic";
 
@@ -16,9 +17,11 @@ async function ListsLayout({
 
   return (
     <div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <div>{children}</div>
-      </HydrationBoundary>
+      <Suspense fallback={<LoadingSpinner isLoading={true} size={48} />}>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <div>{children}</div>
+        </HydrationBoundary>
+      </Suspense>
     </div>
   );
 }
