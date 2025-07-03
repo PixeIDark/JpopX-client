18c4392664222e62f2cd3ef1d4d95041ea815554
PixeIDark <kiki7951@naver.com>
2025-07-04 08:55:42 +0900

    돋보기 클릭으로 검색 기능 추가

--- src/app/(public)/search/_components/SearchPanel/SearchInput/SearchInput.tsx
+++ src/app/(public)/search/_components/SearchPanel/SearchInput/SearchInput.tsx
@@ -2,6 +2,7 @@
 
 import Input from "@/components/ui/Input";
 import { useQueryInput } from "@/hooks/useQueryInput";
+import SearchIcon from "@/assets/icons/search/search.svg";
 
 interface SearchInputProps {
   initialText: string;
@@ -11,13 +12,16 @@ function SearchInput({ initialText }: SearchInputProps) {
   const { handleSubmit, inputRef } = useQueryInput(initialText, "text");
 
   return (
-    <form onSubmit={handleSubmit} className="w-full">
+    <form onSubmit={handleSubmit} className="relative w-full">
       <Input
         placeholder="Please Enter Something"
         ref={inputRef}
         type="text"
         defaultValue={initialText}
       />
+      <button onClick={handleSubmit} className="absolute right-4 top-1/2 -translate-y-1/2">
+        <SearchIcon width="24" height="24" className="fill-icon-stroke" />
+      </button>
     </form>
   );
 }
