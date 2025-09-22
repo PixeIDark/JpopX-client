6b531cf66b733fa0ba08010deba0b1614c69c241
PixeIDark <kiki7951@naver.com>
2025-09-22 22:57:51 +0900

    SongCard.tsx artist, title 텍스트 style 최대 너비 제한 및 매직넘버 제거

--- src/components/common/SongCard/SongCard.tsx
+++ src/components/common/SongCard/SongCard.tsx
@@ -1,11 +1,15 @@
 import { SearchItems } from "@/types/search.type";
 import Picture from "@/components/ui/Picture";
-import AddSongToListButton from "@/components/common/SongCard/AddSongToListButton/AddSongToListButton";
+import AddSongToListButton from "@/components/common/SongCard/AddSongToListButton";
 import { songProcessor } from "@/utils/services/songProcessor";
 
 interface SongCardProps {
   song: SearchItems;
 }
+
+const maxContainerWidth = 512;
+const limitWidth = 152 + (187 - 152) * ((maxContainerWidth - 365) / 35);
+const textMaxWidth = `min(calc(152px + (187 - 152) * ((100vw - 365px) / 35)), ${limitWidth}px)`;
 
 function SongCard({ song }: SongCardProps) {
   const { title, artist, tjNumber, kyNumber } = songProcessor(song);
@@ -22,13 +26,13 @@ function SongCard({ song }: SongCardProps) {
       <div className="ml-3 flex w-full flex-col justify-between">
         <h1
           className="overflow-hidden text-ellipsis whitespace-nowrap text-text-h"
-          style={{ maxWidth: "calc(152px + (187 - 152) * ((100vw - 365px) / 35))" }}
+          style={{ maxWidth: textMaxWidth }}
         >
           {title}
         </h1>
         <p
           className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-text-p"
-          style={{ maxWidth: "calc(152px + (187 - 152) * ((100vw - 365px) / 35))" }}
+          style={{ maxWidth: textMaxWidth }}
         >
           {artist}
         </p>
